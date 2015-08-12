bab2e2568f085342380c1f956fff357fa2809049
Ahmet DAL <ceahmetdal@gmail.com>
2015-08-12 08:56:34 +0300

    hashing now includes keys

--- river/handlers/transition.py
+++ river/handlers/transition.py
@@ -8,7 +8,7 @@ class TransitionHandler(Handler):
     handlers = {}
 
     @classmethod
-    def get_handler(cls, handler, workflow_object, field, source_state=None, destination_state=None, *args, **kwargs):
+    def get_handler(cls, handler, workflow_object, field, source_state=None, destination_state=None, approvement=None, *args, **kwargs):
         d = super(TransitionHandler, cls).get_handler(handler, workflow_object, field, *args, **kwargs)
         if source_state:
             d['source_state_pk'] = source_state.pk
@@ -17,9 +17,9 @@ class TransitionHandler(Handler):
         return d
 
     @classmethod
-    def get_hash(cls, workflow_object, field, source_state=None, destination_state=None, *args, **kwargs):
+    def get_hash(cls, workflow_object, field, source_state=None, destination_state=None, approvement=None, *args, **kwargs):
         return super(TransitionHandler, cls).get_hash(workflow_object, field) + ('source_state' + str(source_state.pk) if source_state else '') + (
-        'destination_state' + str(destination_state.pk) if destination_state else '')
+            'destination_state' + str(destination_state.pk) if destination_state else '') + ('approvement' + str(approvement.pk) if approvement else '')
 
 
 on_transition.connect(TransitionHandler.dispatch)
