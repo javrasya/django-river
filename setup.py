4396e6a0a040770eb6e0d7c076684ffd7f149cfe
Ahmet DAL <ceahmetdal@gmail.com>
2015-08-30 19:44:50 +0300

    version upgrade to 0.3.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='django-river',
-    version='0.3.2',
+    version='0.3.3',
     author='Ahmet DAL',
     author_email='ceahmetdal@gmail.com',
     packages=find_packages(),
