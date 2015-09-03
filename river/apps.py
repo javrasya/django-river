d3bd9b88c475bbe4bc8246b06930f0089e33f3bf
Ahmet DAL <ceahmetdal@gmail.com>
2015-09-03 13:08:53 +0300

    this resolves #13. DatabaseHandlerBackend was trying to access database on initialisation. This is now after database is created.

--- river/apps.py
+++ river/apps.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.apps import AppConfig
+from django.db.utils import OperationalError
 from river.handlers.backends.database import DatabaseHandlerBackend
 from river.handlers.backends.loader import handler_backend
 
@@ -15,5 +16,10 @@ class RiverApp(AppConfig):
 
     def ready(self):
         if isinstance(handler_backend, DatabaseHandlerBackend):
-            handler_backend.initialize_handlers()
+            try:
+                self.get_model('Handler').objects.exists()
+                handler_backend.initialize_handlers()
+            except OperationalError:
+                LOGGER.debug('Database handlers are not registered. Because database is not created yet.')
+
         LOGGER.debug('RiverApp is loaded.')
