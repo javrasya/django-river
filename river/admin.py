3667d1c0f88b3f8a9e07ecd839af9c357e6c4460
George Silva <georger.silva@gmail.com>
2015-09-04 00:40:03 -0300

    adding admin.py to register the concrete models in the admin

---
+++ river/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Handler)
+admin.site.register(Proceeding)
+admin.site.register(ProceedingTrack)
+admin.site.register(State)
+admin.site.register(Transition)
