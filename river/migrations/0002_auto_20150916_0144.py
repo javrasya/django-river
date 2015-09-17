c9f89244104de7ac87310ee7241b06ca6139fca6
Ahmet DAL <ceahmetdal@gmail.com>
2015-09-17 18:03:41 +0300

    migration fix

--- river/migrations/0002_auto_20150916_0144.py
+++ river/migrations/0002_auto_20150916_0144.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='proceedingmeta',
             name='content_type',
-            field=models.ForeignKey(default=-1, verbose_name='Content Type', to='contenttypes.ContentType'),
+            field=models.ForeignKey(default=1, verbose_name='Content Type', to='contenttypes.ContentType'),
             preserve_default=False,
         ),
         migrations.AddField(
