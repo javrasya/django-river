c605a6a5c6331fde39aebb8adb6da59279f6dea7
Ahmet DAL <ceahmetdal@gmail.com>
2016-04-29 02:44:08 +0300

    version is upgraded to 0.8.0 :arrow_up:

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except IOError as err:
 
 setup(
     name='django-river',
-    version='0.7.0',
+    version='0.8.0',
     author='Ahmet DAL',
     author_email='ceahmetdal@gmail.com',
     packages=find_packages(),
