0dac19df724a174431916cf71fbe867083603baa
Ahmet DAL <ahmet.dal@klarna.com>
2019-01-04 20:50:26 +0100

    Fix loading fixtures

--- river/models/managers/transitionmetada.py
+++ river/models/managers/transitionmetada.py
@@ -4,5 +4,5 @@ __author__ = 'ahmetdal'
 
 
 class TransitionApprovalMetadataManager(models.Manager):
-    def get_by_natural_key(self, content_type, transition, priority):
-        return self.get(content_type=content_type, transition=transition, priority=priority)
+    def get_by_natural_key(self, field_name, content_type, source_state, destination_state, priority):
+        return self.get(field_name=field_name, content_type=content_type, source_state=source_state, destination_state=destination_state, priority=priority)
