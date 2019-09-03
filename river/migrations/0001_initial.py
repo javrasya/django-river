bd5db2f2198099b8a4f67407718f48c0be890405
Ahmet DAL <ahmet.dal@klarna.com>
2019-09-03 21:43:17 +0200

    [#92] Fix migration

--- river/migrations/0001_initial.py
+++ river/migrations/0001_initial.py
@@ -11,9 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('auth', '0009_alter_user_last_name_max_length'),
     ]
 
     operations = [
