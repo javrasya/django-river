8753d40ba296302edb7bc38fa1db1e23e4e5d015
Ahmet DAL <ceahmetdal@gmail.com>
2019-09-05 11:53:47 +0200

    [#94] Support class level hooking (#99)

--- river/tests/hooking/test__completed_hooking.py
+++ river/tests/hooking/test__completed_hooking.py
@@ -14,7 +14,7 @@ class CompletedHookingTest(TestCase):
     def setUp(self):
         super(CompletedHookingTest, self).setUp()
 
-    def test_shouldInvokeTheRegisteredCallBackWhenFlowIsCompleteForTheObject(self):
+    def test_shouldInvokeTheRegisteredViaInstanceApiCallBackWhenFlowIsCompleteForTheObject(self):
         authorized_permission = PermissionObjectFactory()
         authorized_user = UserObjectFactory(user_permissions=[authorized_permission])
 
@@ -50,6 +50,56 @@ class CompletedHookingTest(TestCase):
             self.test_kwargs = kwargs
 
         workflow_object.model.river.my_field.hook_post_complete(test_callback)
+
+        assert_that(self.test_args, none())
+
+        assert_that(workflow_object.model.my_field, equal_to(state1))
+        workflow_object.model.river.my_field.approve(as_user=authorized_user)
+        assert_that(workflow_object.model.my_field, equal_to(state2))
+
+        assert_that(self.test_args, none())
+
+        workflow_object.model.river.my_field.approve(as_user=authorized_user)
+        assert_that(workflow_object.model.my_field, equal_to(state3))
+
+        assert_that(self.test_args, equal_to((workflow_object.model, "my_field")))
+
+    def test_shouldInvokeTheRegisteredViaClassApiCallBackWhenFlowIsCompleteForTheObject(self):
+        authorized_permission = PermissionObjectFactory()
+        authorized_user = UserObjectFactory(user_permissions=[authorized_permission])
+
+        state1 = StateObjectFactory(label="state1")
+        state2 = StateObjectFactory(label="state2")
+        state3 = StateObjectFactory(label="state3")
+
+        content_type = ContentType.objects.get_for_model(BasicTestModel)
+        workflow = WorkflowFactory(initial_state=state1, content_type=content_type, field_name="my_field")
+        TransitionApprovalMetaFactory.create(
+            workflow=workflow,
+            source_state=state1,
+            destination_state=state2,
+            priority=0,
+            permissions=[authorized_permission]
+        )
+
+        TransitionApprovalMetaFactory.create(
+            workflow=workflow,
+            source_state=state2,
+            destination_state=state3,
+            priority=0,
+            permissions=[authorized_permission]
+        )
+
+        workflow_object = BasicTestModelObjectFactory()
+
+        self.test_args = None
+        self.test_kwargs = None
+
+        def test_callback(*args, **kwargs):
+            self.test_args = args
+            self.test_kwargs = kwargs
+
+        BasicTestModel.river.my_field.hook_post_complete(test_callback)
 
         assert_that(self.test_args, none())
 
--- river/tests/hooking/test__transition_hooking.py
+++ river/tests/hooking/test__transition_hooking.py
@@ -13,7 +13,7 @@ __author__ = 'ahmetdal'
 # noinspection DuplicatedCode
 class TransitionHooking(TestCase):
 
-    def test_shouldInvokeTheRegisteredCallBackWhenATransitionHappens(self):
+    def test_shouldInvokeTheRegisteredViaInstanceApiCallBackWhenATransitionHappens(self):
         self.test_args = None
         self.test_kwargs = None
 
@@ -64,7 +64,58 @@ class TransitionHooking(TestCase):
         last_approval = TransitionApproval.objects.get(object_id=workflow_object.model.pk, source_state=state1, destination_state=state2, priority=1)
         assert_that(self.test_kwargs, has_entry(equal_to("transition_approval"), equal_to(last_approval)))
 
-    def test_shouldInvokeTheRegisteredCallBackWhenASpecificTransitionHappens(self):
+    def test_shouldInvokeTheRegisteredViaClassApiCallBackWhenATransitionHappens(self):
+        self.test_args = None
+        self.test_kwargs = None
+
+        def test_callback(*args, **kwargs):
+            self.test_args = args
+            self.test_kwargs = kwargs
+
+        authorized_permission = PermissionObjectFactory()
+        authorized_user = UserObjectFactory(user_permissions=[authorized_permission])
+
+        state1 = StateObjectFactory(label="state1")
+        state2 = StateObjectFactory(label="state2")
+
+        content_type = ContentType.objects.get_for_model(BasicTestModel)
+        workflow = WorkflowFactory(initial_state=state1, content_type=content_type, field_name="my_field")
+        TransitionApprovalMetaFactory.create(
+            workflow=workflow,
+            source_state=state1,
+            destination_state=state2,
+            priority=0,
+            permissions=[authorized_permission]
+        )
+
+        TransitionApprovalMetaFactory.create(
+            workflow=workflow,
+            source_state=state1,
+            destination_state=state2,
+            priority=1,
+            permissions=[authorized_permission]
+        )
+
+        workflow_object = BasicTestModelObjectFactory()
+
+        BasicTestModel.river.my_field.hook_post_transition(test_callback)
+
+        assert_that(self.test_args, none())
+
+        assert_that(workflow_object.model.my_field, equal_to(state1))
+        workflow_object.model.river.my_field.approve(as_user=authorized_user)
+        assert_that(workflow_object.model.my_field, equal_to(state1))
+
+        assert_that(self.test_args, none())
+
+        workflow_object.model.river.my_field.approve(as_user=authorized_user)
+        assert_that(workflow_object.model.my_field, equal_to(state2))
+        assert_that(self.test_args, equal_to((workflow_object.model, "my_field")))
+
+        last_approval = TransitionApproval.objects.get(object_id=workflow_object.model.pk, source_state=state1, destination_state=state2, priority=1)
+        assert_that(self.test_kwargs, has_entry(equal_to("transition_approval"), equal_to(last_approval)))
+
+    def test_shouldInvokeTheRegisteredViaInstanceApiCallBackWhenASpecificTransitionHappens(self):
         self.test_args = None
         self.test_kwargs = None
 
@@ -100,6 +151,58 @@ class TransitionHooking(TestCase):
         workflow_object = BasicTestModelObjectFactory()
 
         workflow_object.model.river.my_field.hook_post_transition(test_callback, source_state=state2, destination_state=state3)
+
+        assert_that(self.test_args, none())
+
+        assert_that(workflow_object.model.my_field, equal_to(state1))
+        workflow_object.model.river.my_field.approve(as_user=authorized_user)
+        assert_that(workflow_object.model.my_field, equal_to(state2))
+
+        assert_that(self.test_args, none())
+
+        workflow_object.model.river.my_field.approve(as_user=authorized_user)
+        assert_that(workflow_object.model.my_field, equal_to(state3))
+        assert_that(self.test_args, equal_to((workflow_object.model, "my_field")))
+
+        last_approval = TransitionApproval.objects.get(object_id=workflow_object.model.pk, source_state=state2, destination_state=state3)
+        assert_that(self.test_kwargs, has_entry(equal_to("transition_approval"), equal_to(last_approval)))
+
+    def test_shouldInvokeTheRegisteredViaClassApiCallBackWhenASpecificTransitionHappens(self):
+        self.test_args = None
+        self.test_kwargs = None
+
+        def test_callback(*args, **kwargs):
+            self.test_args = args
+            self.test_kwargs = kwargs
+
+        authorized_permission = PermissionObjectFactory()
+        authorized_user = UserObjectFactory(user_permissions=[authorized_permission])
+
+        state1 = StateObjectFactory(label="state1")
+        state2 = StateObjectFactory(label="state2")
+        state3 = StateObjectFactory(label="state3")
+
+        content_type = ContentType.objects.get_for_model(BasicTestModel)
+        workflow = WorkflowFactory(initial_state=state1, content_type=content_type, field_name="my_field")
+        TransitionApprovalMetaFactory.create(
+            workflow=workflow,
+            source_state=state1,
+            destination_state=state2,
+            priority=0,
+            permissions=[authorized_permission]
+        )
+
+        TransitionApprovalMetaFactory.create(
+            workflow=workflow,
+            source_state=state2,
+            destination_state=state3,
+            priority=0,
+            permissions=[authorized_permission]
+        )
+
+        workflow_object = BasicTestModelObjectFactory()
+
+        BasicTestModel.river.my_field.hook_post_transition(test_callback, source_state=state2, destination_state=state3)
 
         assert_that(self.test_args, none())
 
