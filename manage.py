82a9d2239044708df25e8f3d4d38923d473c5f12
Ahmet DAL <ceahmetdal@gmail.com>
2019-11-30 18:19:55 +0100

    Unignore settings with sqlite3

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import os
 import sys
 
 if __name__ == "__main__":  
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.with_sqlite3")
 
     from django.core.management import execute_from_command_line
 
