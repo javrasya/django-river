83c75606c6774294f445be9b57a15338b2696cd2
Ahmet DAL <ceahmetdal@gmail.com>
2020-03-16 12:29:43 +0100

    Reset migrations

--- river/config.py
+++ river/config.py
@@ -1,5 +1,3 @@
-from functools import lru_cache
-
 from django.contrib.auth.models import Permission, Group
 from django.contrib.contenttypes.models import ContentType
 
@@ -9,24 +7,30 @@ from django.db import connection
 class RiverConfig(object):
     prefix = 'RIVER'
 
+    def __init__(self):
+        self.cached_settings = None
+
     @property
-    @lru_cache()
     def settings(self):
-        from django.conf import settings
-        allowed_configurations = {
-            'CONTENT_TYPE_CLASS': ContentType,
-            'USER_CLASS': settings.AUTH_USER_MODEL,
-            'PERMISSION_CLASS': Permission,
-            'GROUP_CLASS': Group,
-            'INJECT_MODEL_ADMIN': False
-        }
-        river_settings = {}
-        for key, default in allowed_configurations.items():
-            river_settings[key] = getattr(settings, self.get_with_prefix(key), default)
+        if self.cached_settings:
+            return self.cached_settings
+        else:
+            from django.conf import settings
+            allowed_configurations = {
+                'CONTENT_TYPE_CLASS': ContentType,
+                'USER_CLASS': settings.AUTH_USER_MODEL,
+                'PERMISSION_CLASS': Permission,
+                'GROUP_CLASS': Group,
+                'INJECT_MODEL_ADMIN': False
+            }
+            river_settings = {}
+            for key, default in allowed_configurations.items():
+                river_settings[key] = getattr(settings, self.get_with_prefix(key), default)
 
-        river_settings['IS_MSSQL'] = connection.vendor == 'microsoft'
+            river_settings['IS_MSSQL'] = connection.vendor == 'microsoft'
+            self.cached_settings = river_settings
 
-        return river_settings
+            return self.cached_settings
 
     def get_with_prefix(self, config):
         return '%s_%s' % (self.prefix, config)
