ebbd5f6bbdabdf92d33249c7ec0d1cdea6124473
Ahmet DAL <ceahmetdal@gmail.com>
2020-03-17 15:34:04 +0100

    :arrow_up: Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except IOError as err:
 
 setup(
     name='django-river',
-    version='3.1.4',
+    version='3.2.0',
     author='Ahmet DAL',
     author_email='ceahmetdal@gmail.com',
     packages=find_packages(),
